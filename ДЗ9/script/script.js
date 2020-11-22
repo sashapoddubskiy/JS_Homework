a69fd242898710e0ebabae147ff4f112e6d531cb
sashapoddubskiy <poddubich@live.com>
2020-11-22 09:36:32 +0300

    creating main structure

---
+++ ДЗ9/script/script.js
@@ -0,0 +1,37 @@
+function Cat(name) {
+    var foodAmount = 50;
+
+    function formatFoodAmount() {
+        return foodAmount + ' гр.';
+    }
+
+    this.dailyNorm = function(amount) {
+        if (!arguments.length) return formatFoodAmount();
+
+        if (amount < 50 || amount > 500) {
+            return 'Недопустимое количество корма.';
+        }
+
+        foodAmount = amount;
+    };
+
+    this.name = name;
+
+    this.feed = function() {
+        console.log('Насыпаем в миску ' + this.dailyNorm() + ' корма.');
+    };
+}
+
+var barsik = new Cat('Барсик');
+
+console.log(barsik.name);
+
+console.log(barsik.dailyNorm());
+console.log(barsik.feed());
+
+console.log(barsik.dailyNorm(600));
+console.log(barsik.feed());
+
+console.log(barsik.dailyNorm(250));
+console.log(barsik.feed());
+
