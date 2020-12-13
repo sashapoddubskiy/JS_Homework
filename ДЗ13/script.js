747549ebf430654f15b0d610586aca645d49ca25
sashapoddubskiy <poddubich@live.com>
2020-12-13 16:25:28 +0300

    input, addRow

--- ДЗ13/script.js
+++ ДЗ13/script.js
@@ -1,3 +1,4 @@
+//inputer();
 var d = document;
 
 function addRow()
@@ -16,5 +17,46 @@ function addRow()
     row.appendChild(td1);
     row.appendChild(td2);
     row.appendChild(td3);
+    //inputer();
+}
 
+function inputer(){
+
+var tds = document.querySelectorAll('td');
+
+for (var i = 0; i < tds.length; i++) {
+	tds[i].addEventListener('click', function func() {
+		var input = document.createElement('input');
+		input.value = this.innerHTML;
+		this.innerHTML = '';
+		this.appendChild(input);
+		
+		var td = this;
+		input.addEventListener('blur', function() {
+			td.innerHTML = this.value;
+			td.addEventListener('click', func);
+		});
+		
+		this.removeEventListener('click', func);
+	});
+}
+}
+
+var tds = document.querySelectorAll('td');
+
+for (var i = 0; i < tds.length; i++) {
+	tds[i].addEventListener('click', function func() {
+		var input = document.createElement('input');
+		input.value = this.innerHTML;
+		this.innerHTML = '';
+		this.appendChild(input);
+		
+		var td = this;
+		input.addEventListener('blur', function() {
+			td.innerHTML = this.value;
+			td.addEventListener('click', func);
+		});
+		
+		this.removeEventListener('click', func);
+	});
 }
